d30df97518ddd6967dfd4b407b0c69875fece6c1
SaeedMrz <saeedmirzaee7091@gmail.com>
2022-10-30 11:47:07 -0700

    first

---
+++ main.js
@@ -0,0 +1,25 @@
+const canvas = document.querySelector("#myCanvas");
+
+canvas.width = 200;
+
+// to draw a canvass first of all we need to get the drawing context
+const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+
+animate();
+
+function animate() {
+  car.update(road.borders);
+
+  canvas.height = window.innerHeight;
+
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+
+  road.draw(ctx);
+  car.draw(ctx);
+
+  ctx.restore();
+  requestAnimationFrame(animate);
+}
